fc2dca4a50f7f7859d814ef3eb8de40e5cca1cf7
TheBugCrafter-1 <witcher000000000@gmail.com>
2026-01-13 16:47:14 +0530

    i changed the print function to return function so that i can reuse the stored data in my code

--- RPS GAME/RPSGame.py
+++ RPS GAME/RPSGame.py
@@ -7,6 +7,7 @@ print("===================")
 computers = ['rock', 'paper', 'scissors'] 
 Player_choice = input("Select One Option: ROCK, PAPER, SCISORR: ")
 
+
 computer_choice = random.choice(computers)
 print("Computer chose:", computer_choice)
 
@@ -17,19 +18,17 @@ print(f'Player Choose: {PlayerChoice}')
 
 def WhoWin(computer_choice, Player_choice):
     if computer_choice == Player_choice:
-        print("THIS IS DRAWWW!!!")
+        return 'This is a D R A W W W !!!'
     elif computer_choice == "rock" and Player_choice == "scissors":
-        print(f"Computer W I N ")
+        return 'computer W I N !'
     elif computer_choice == "paper" and Player_choice == 'scissors':
-        print(f"Player W I N ")
+        return"player W I N "
     elif computer_choice == 'scissors' and Player_choice == 'rock':
-        print("Player W I N")
+        return"Player W I N"
     else:
-        print("syntex error")
-
+        return"syntex error"
 
 WhoWin(computer_choice, Player_choice )
-
 
  
 
