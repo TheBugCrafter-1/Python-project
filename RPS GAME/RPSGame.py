950e298c62a26b2c4f1e68b24b5ed67e0a12a5c0
TheBugCrafter-1 <witcher000000000@gmail.com>
2026-01-13 13:34:21 +0530

    the logic of win and lose is fixed

--- RPS GAME/RPSGame.py
+++ RPS GAME/RPSGame.py
@@ -1,26 +1,34 @@
 import random 
 
-
 print("===================")
 print('Rock Paper Scissors')
 print("===================")
 
 computers = ['rock', 'paper', 'scissors'] 
-Player_choice = input("Select One Option: ROCK, PAPER, SCISORR")
+Player_choice = input("Select One Option: ROCK, PAPER, SCISORR: ")
 
 computer_choice = random.choice(computers)
 print("Computer chose:", computer_choice)
 
 
-if Player_choice == computer_choice:
-    print("THIS IS DRAWWWW!!!")
+PlayerChoice = Player_choice
+print(f'Player Choose: {PlayerChoice}')
 
-elif Player_choice == "rock" and computer_choice == "scissor":
-    print("Player wins")
-elif Player_choice == 'rock' and computer_choice == "paper":
-    print('Computer wins')
-elif Player_choice == "paper" and computer_choice == "rock":
-    print('Player wins')
+
+def WhoWin(computer_choice, Player_choice):
+    if computer_choice == Player_choice:
+        print("THIS IS DRAWWW!!!")
+    elif computer_choice == "rock" and Player_choice == "scissors":
+        print(f"Computer W I N ")
+    elif computer_choice == "paper" and Player_choice == 'scissors':
+        print(f"Player W I N ")
+    elif computer_choice == 'scissors' and Player_choice == 'rock':
+        print("Player W I N")
+    else:
+        print("syntex error")
+
+
+WhoWin(computer_choice, Player_choice )
 
 
  
